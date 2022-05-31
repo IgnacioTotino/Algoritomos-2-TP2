42aa4da16581fc9de415c063b3f3cdd40a9a4bf7
Tomas Aizenscher <tomasaizenscher@Tomass-MacBook-Pro.local>
2022-05-31 19:34:09 -0300

    solucion a simbolos dubplicados de Clear()

--- Casillero.cpp
+++ Casillero.cpp
@@ -103,3 +103,17 @@ void Casillero::eliminarEjercito(){
     this->definirEstado(INHABILITADO);
     this->definirPropiedad(NULL);
 }
+
+extern void Clear()
+{
+
+#if defined _WIN32
+    system("cls");
+    //clrscr(); // including header file : conio.h
+#elif defined (__LINUX__) || defined(__gnu_linux__) || defined(__linux__)
+    system("clear");
+    //std::cout<< u8"\033[2J\033[1;1H"; //Using ANSI Escape Sequences 
+#elif defined (__APPLE__)
+    system("clear");
+#endif
+}
